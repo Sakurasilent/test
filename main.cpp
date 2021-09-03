309960d69166ed67de34e5824c5520bc85703b7f
Sakurasilent <492627639@qq.com>
2021-09-03 16:57:54 +0800

    这是演示一下

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+    printf("++□");
+    cout<<"123"<<endl;
+    cout<<"一二三456"<<endl;
+    system("pause");
+    return 0;
+}
